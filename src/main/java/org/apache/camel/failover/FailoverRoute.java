e01e924a0c018fc6ed9f35ec6094c51291f4b806
Preben Asmussen <pras@dr.dk>
2020-05-06 11:13:27 +0200

    upgrade example to camel 3.2.0 and sb to 2.2.6.RELEASE

--- src/main/java/org/apache/camel/failover/FailoverRoute.java
+++ src/main/java/org/apache/camel/failover/FailoverRoute.java
@@ -1,18 +1,18 @@
 package org.apache.camel.failover;
 
-import org.apache.camel.spring.SpringRouteBuilder;
+import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
-class FailoverRoute extends SpringRouteBuilder  {
+class FailoverRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
         from("timer:foo")
-            .setBody().simple("foo")
+            .setBody()
+            .simple("foo")
             .to("activemq:thequeue");
-        
-    }
 
+    }
 
 }
